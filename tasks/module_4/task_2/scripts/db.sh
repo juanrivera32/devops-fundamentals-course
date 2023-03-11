c76e9642426c0750a0af0cb8e47aad659ffdf7d9
Juan Rivera <guillerivera32@gmail.com>
2023-03-11 17:32:55 -0500

    add missing methods

--- tasks/module_4/task_2/scripts/db.sh
+++ tasks/module_4/task_2/scripts/db.sh
@@ -5,7 +5,6 @@ typeset fileDir=../data
 typeset filePath=$fileDir/$fileName
 
 function checkIfFileExists {
-	echo $filePath
 	if [ -f $filePath ]
 	then
 		return 1
@@ -57,6 +56,50 @@ function add {
 	echo "${username}, ${role}" | tee -a $filePath
 }
 
+function backup {
+  checkIfFileExists
+  cat $filePath > "$fileDir/$(date +%F_%H-%M-%S)-users.db.backup"
+}
+
+function restore {
+  backupFile=$(ls $fileDir/*-$fileName.backup | tail -n 1)
+  
+  if [[ ! -f $backupFile ]]
+  then
+    echo "No backup file found"
+  else
+    cat $backupFile > $filePath
+  fi 
+}
+
+function find {
+  checkIfFileExists
+
+  read -p "Enter username: " username
+  
+  # It feels a little hacky as I didn't find another way to print each entry in a different line
+  if [[ $(grep $username $filePath) == "" ]]
+  then
+    echo "User not found"
+  else
+    grep $username $filePath >> temp.db
+    cat temp.db
+    rm temp.db
+  fi
+  
+}
+
+echo $2
+inverseParam="$2"
+function list {
+    if [[ $inverseParam == "--inverse" ]]
+    then
+      cat -n $filePath | tac
+    else
+      cat -n $filePath
+    fi
+}
+
 function help {
 	checkIfFileExists
 
@@ -66,21 +109,21 @@ function help {
   echo
   echo "List of available commands:"
   echo
-  echo "add       Adds a new line to the users.db. Script must prompt user to type a
+  echo "add     Adds a new line to the users.db. Script must prompt user to type a
         username of new entity. After entering username, user must be prompted to
-        type a role."
-  echo "backup    Creates a new file, named  "$filePath".backup which is a copy of current " $fileName
-  echo "find      Prompts user to type a username, then prints username and role if such
+       	type a role." 
+  echo "backup  Creates a new file, named  "$filePath".backup which is a copy of current " $fileName
+  echo "find    Prompts user to type a username, then prints username and role if such
         exists in users.db. If there is no user with selected username, script must print:
         “User not found”. If there is more than one user with such username, print all
         found entries."
-  echo "list      Prints contents of users.db in format: N. username, role
+  echo "list    Prints contents of users.db in format: N. username, role
         where N – a line number of an actual record
         Accepts an additional optional parameter inverse which allows to get
         result in an opposite order – from bottom to top"
 }
 
-export $1
+# export $1
 
 case $1 in
   add)            add ;;
