41aa2cc2d7a94f2fb72e04600d4b7d9a081b61be
Juan Rivera <guillerivera32@gmail.com>
2023-03-22 00:01:15 -0500

    wip: adding extra tasks

--- lab_1/scripts/update-pipeline-definition.sh
+++ lab_1/scripts/update-pipeline-definition.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
 
+# colors
+red=$'\e[1;31m'
+grn=$'\e[1;32m'
+#yel=$'\e[1;33m'
+#blu=$'\e[1;34m'
+#mag=$'\e[1;35m'
+#cyn=$'\e[1;36m'
+end=$'\e[0m'
+
+checkJQ() {
+  # jq test
+  type jq >/dev/null 2>&1
+  exitCode=$?
+
+  echo $exitCode;
+  if [ "$exitCode" -ne 0 ]; then
+    printf "  ${red}'jq' not found! (json parser)\n${end}"
+    printf "    Ubuntu Installation: sudo apt install jq\n"
+    printf "    Redhat Installation: sudo yum install jq\n"
+    jqDependency=0
+  elif [[ "$DEBUG" -eq 1 ]]; then
+    printf "  ${grn}'jq' found!\n${end}"
+  fi
+}
+
+checkJQ
+
+current_json=$1
+
+if [ ! -f "$current_json" ]; then
+    echo "${red}$current_json is not a file or does not exist"
+    exit 1;
+fi
+
 while [ ! -z "$1" ]; do
   case "$1" in
      --branch)
@@ -14,17 +48,22 @@ while [ ! -z "$1" ]; do
         shift
         pollForSourceChanges=$1
          ;;
+    --configuration)
+         shift
+         configuration=$1
+         ;;
   esac
 shift
 done
 
 branch="${branch:=main}"
 pollForSourceChanges="${pollForSourceChanges:=false}"
+
 echo ">>>>>> $pollForSourceChanges"
 
 echo "Creating copy of pipeline.json file"
 echo ""
-current_json=../pipeline.json
+
 new_json_file="../pipeline$(date +%F_%H-%M-%S).json"
 # touch $new_json_file
 # cp ../pipeline.json $current_json
@@ -35,8 +74,12 @@ new_json_file="../pipeline$(date +%F_%H-%M-%S).json"
 # Increment version by one
 # jq '. |= del(.metadata) | .pipeline.version |= . + 1' $current_json >> $new_json_file
 
-echo $branch
-# set branch property
-jq --arg branch $branch --arg owner $owner --arg pollForSourceChanges $pollForSourceChanges '. |= del(.metadata) | .pipeline.version |= . + 1 | .pipeline.stages[0].actions[0].configuration.Branch |= $branch | .pipeline.stages[0].actions[0].configuration.Owner |= $owner | .pipeline.stages[0].actions[0].configuration.PollForSourceChanges |= $pollForSourceChanges' $current_json >> $new_json_file
+# jq '.. | .EnvironmentVariables?'
+# jq '(.. | .configuration?.EnvironmentVariables?) |= "XXXX"' $current_json
+
+# jq --arg configuration $configuration '.. | .EnvironmentVariables? | select(. != null) | gsub("{{BUILD_CONFIGURATION value}}";$configuration)' $current_json
+
+
+jq --arg configuration $configuration --arg branch $branch --arg owner $owner --arg pollForSourceChanges $pollForSourceChanges '. |= del(.metadata) | .pipeline.version |= . + 1 | .pipeline.stages[0].actions[0].configuration.Branch |= $branch | .pipeline.stages[0].actions[0].configuration.Owner |= $owner | .pipeline.stages[0].actions[0].configuration.PollForSourceChanges |= $pollForSourceChanges | .. | .EnvironmentVariables? | select(. != null) | sub("{{BUILD_CONFIGURATION value}}";$configuration)' $current_json >> $new_json_file
 
 
